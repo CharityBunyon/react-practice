2895f59bedc9436e4c138441aeef4a5291c0a122
Charity Bunyon <CSEYMOU5@GMAIL.com>
2020-03-24 09:32:46 -0500

    setup complete

--- src/App/App.js
+++ src/App/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+import './App.scss';
 
 function App() {
   return (
@@ -19,6 +19,8 @@ function App() {
           Learn React
         </a>
       </header>
+
+      <button className="btn btn-danger">Hello</button>
     </div>
   );
 }
