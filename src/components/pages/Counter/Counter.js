0456b1ed8eba9ccb2f371404e929c3f964a0260f
Charity Bunyon <CSEYMOU5@GMAIL.com>
2020-06-12 09:50:59 -0500

    rendering lists

--- src/components/pages/Counter/Counter.js
+++ src/components/pages/Counter/Counter.js
@@ -3,26 +3,26 @@ import './Counter.scss';
 
 class Counter extends React.Component {
   state = {
-    count: 1,
-    // imageUrl: 'https://picsum.photos/200',
+    count: 0,
+    tags: ['tag1', 'tag2', 'tag3'],
   }
 
-  // styles = {
-  //   fontSize: 10,
-  //   fontWeight: 'bold',
-  // }
-
   render() {
-    let classes = ' badge, m-2 badge-';
-    classes += (this.state.count === 0) ? 'warning' : 'primary';
-
     return (
             <div>
-              {/* <img src={this.state.imageUrl} alt=""/> */}
-                <span className={classes}>{this.formatCount()}</span>
+                <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
                 <button className="btn btn-secondary btn-sm">Increment</button>
+                <ul>
+                  {this.state.tags.map((tag) => <li key={tag}>{ tag }</li>)}
+                </ul>
             </div>
     );
+  }
+
+  getBadgeClasses() {
+    let classes = ' badge, m-2 badge-';
+    classes += (this.state.count === 0) ? 'warning' : 'primary';
+    return classes;
   }
 
   formatCount() {
