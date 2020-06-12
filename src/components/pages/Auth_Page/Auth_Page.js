b1b35d820c14f94b849b9cb8f9c7c650eb7563f8
Charity Bunyon <CSEYMOU5@GMAIL.com>
2020-06-12 08:42:38 -0500

    changing styles with terbary operator

---
+++ src/components/pages/Auth_Page/Auth_Page.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+import './Auth_page.scss';
+
+const first = [1, 2, 3];
+const second = [4, 5, 6];
+
+const combine = [...first, ...second];
+console.error(combine);
+
+class Auth extends React.Component {
+  render() {
+    return (
+        <div>Hello</div>
+
+    );
+  }
+}
+
+export default Auth;
